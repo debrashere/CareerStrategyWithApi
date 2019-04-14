0844096e16a8a7c6f181d5bdc90d2f49b3f9a1d2
debrashere <debrashere@gmail.com>
2019-04-14 11:21:36 -0700

    update prospect summary layout

--- public/content/js/lib/jobs-summary.js
+++ public/content/js/lib/jobs-summary.js
@@ -17,53 +17,18 @@ function generateJobSummaries(prospects) {
      ${dateWhen.getHours()}:${dateWhen.getMinutes()}:${dateWhen.getSeconds()} `;            
       
   summaries += `
-  <div class="section-block">  
-    <div class="section-sub-header"><a id="prospectId-${index}" href=# class="js-prospect-detail">${prospect.what}</a></div>
-    <div>${prospect.where}</div>   
-    <div>${formattedDate}</div>
-    <div>${prospect.status}</div>
-    <div hidden><span class="js-prospectId" id="Prospect-${index}" hidden>${prospect.id}</span></div>
+  <div class="flex-container section-block">  
+    <div class="flex-item section-sub-header"><a id="prospectId-${index}" href=# class="js-prospect-detail">${prospect.what}</a></div>
+    <div class="flex-item">${prospect.where}</div>   
+    <div class="flex-item">${formattedDate}</div>
+    <div class="flex-item">${prospect.status}</div>
+    <div class="flex-item" hidden><span class="js-prospectId" id="Prospect-${index}" hidden>${prospect.id}</span></div>
   </div>`;
   });    
 
   return summaries;  
 }
-
-function generateJobSummariesOLD(prospects) {        
-  let tableRows =  '';
-
-  prospects.map( function(prospect, index) {  
-    let dateWhen = new Date(prospect.when);
-    let formattedDate = `${dateWhen.getMonth()}/${dateWhen.getDay()}/${dateWhen.getFullYear()}  - 
-     ${dateWhen.getHours()}:${dateWhen.getMinutes()}:${dateWhen.getSeconds()} `;            
-      
-  tableRows += `
-  <div class="tr">  
-    <div class="td" data-header="Title"><a id="prospectId-${index}" href=# class="js-prospect-detail">${prospect.what}</a></div>
-    <div class="td" data-header="Company"> ${prospect.where}</div>
-    <div class="td" data-header="When">${formattedDate}</div>
-    <div class="td" data-header="Status">${prospect.status}</div>
-    <div class="td" hidden><span class="js-prospectId" id="Prospect-${index}" hidden>${prospect.id}</span></div>
-  </div>`;
-  });    
-
-  let table =   `
-  <div class="flex-item-skills">
-    <div class="section-header"><h3 tabindex="0">Job Prospects</h3></div>
-    <output><div id="js-skills-error-message" class="error-message" aria-live="assertive" hidden> </div></output>
-    <div class="table">
-      <div class="tr th"> 
-        <div class="td">Title</div> 
-        <div class="td">Company</div>
-        <div class="td">When</div> 
-        <div class="td">Status</div>    
-      </div>
-      ${tableRows}   
-    </div> `;
-
-  return table;  
-}
-
+ 
 function renderJobsSummariesByStatus() {  
   if (!isUserLoggedIn() || !props.PROSPECTS) return;
 
